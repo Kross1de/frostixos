768a4f81f0e4b0a71f778d0a7ad82c56ccabd608
Kross1de <awesome.hubygu@yandex.ru>
2025-08-30 11:56:52 +0600

    small cleanup

--- include/lib/libc/stddef.h
+++ include/lib/libc/stddef.h
@@ -1,8 +1,6 @@
 #ifndef STDDEF_H
 #define STDDEF_H
 
-#include <kernel/kernel.h>
-
 typedef unsigned long size_t;
 typedef long ptrdiff_t;
 #define NULL ((void*)0)
--- include/lib/libc/stdint.h
+++ include/lib/libc/stdint.h
@@ -1,8 +1,6 @@
 #ifndef STDINT_H
 #define STDINT_H
 
-#include <kernel/kernel.h>
-
 typedef unsigned char      uint8_t;
 typedef unsigned short     uint16_t;
 typedef unsigned int       uint32_t;
