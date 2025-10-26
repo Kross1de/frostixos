5a56cd93a116abae51e12519423ab8902d28007f
Kross1de <awesome.hubygu@yandex.ru>
2025-10-26 23:11:17 +0600

    [initrd] remove useless from initrd_dir

--- initrd_dir/src/arch/i386/idt_flush.s
+++
@@ -1,9 +0,0 @@
-section .text
-global idt_flush
-
-; idt_flush: load IDTR
-; arg: pointer to idt_ptr on stack
-idt_flush:
-	mov	eax, [esp + 4]
-	lidt	[eax]
-	ret
--- initrd_dir/src/kernel/symbols.c
+++
@@ -1,8 +0,0 @@
-#include <kernel/kernel.h>
-#include <stddef.h>
-
-struct symbol { u32 addr; const char *name; };
-struct symbol symbols[] = {
-    /* legacy table empty */
-};
-size_t num_symbols = 0;
